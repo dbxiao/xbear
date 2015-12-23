1ac5c4b434c85521721df5bc303e2528f4dd7fd1
unknown <doublexiao@qq.com>
2015-12-23 14:46:26 +0800

    updata

--- config/default.js
+++ config/default.js
@@ -2,80 +2,80 @@
  * xbear配置文件，请勿修改，路由问题，请联系：dbxiao@foxmail.com
  */
 
-var now = new Date();
-var time = [now.getFullYear(), now.getMonth()+1, now.getDate(), now.getHours() , now.getMinutes(), now.getSeconds()].join("");
+// var now = new Date();
+// var time = [now.getFullYear(), now.getMonth()+1, now.getDate(), now.getHours() , now.getMinutes(), now.getSeconds()].join("");
 
-module.exports = {
-    project: {
-        md5Length: 8,
-        md5Connector: '.'
-    },
-    modules : {
-        parser : {
-            css : ["less"]
-        },
-        postpackager : "simple"
-    },
-    settings : {
-        postprocessor : {
-            jswrapper : {
-                type:'amd'
-            }
-        },
-        optimizer : {
-            'png-compressor' : {
-                type : 'pngquant',
-                speed : 2,
-                quality : [80, 100],
-                iebug : false
-            },
-            "uglify-js" : { 
-                mangle: {
-                    except: "exports, module, require, define"
-                }
-            }
-        }
-    },
-    roadmap:{
-        path:[
-            /*** 模板编译规则（将所有html文件发布到view目录）*/
-            {
-                reg : /^\/(page|layout|widget)\/(.+\.html)$/i,
-                isMod : true,
-                release : '/view/${product}/${namespace}/$1/$2',
-                id : '$1/$2'
-            },
-            /*** 静态文件编译规则（将page、layout、widget静态文件发布到res目录）*/
-            {
-                reg : /^\/(page|layout|widget)\/(.*\.(js|css))$/i,
-                isMod : true,
-                release : '/res/${product}/${namespace}/$1/$2',
-                id : '$1/$2',
-                query : "?t="+time
-            },
-            /** page和widget doc目录下的ue设计图、mrd不需要编译产出 */
-            {
-                reg : /^\/(page|widget)\/.*\/doc\/[^\.]*\.(psd|png|jpg|doc|md)/i,
-                release : false
-            },
-            /** 匹配static下的所有资源 */
-            {
-                reg : /^\/static\/(.*\/ls-conf\.js)$/i,
-                release : '/static/${product}/${namespace}/$1',
-                isLSLib : true,
-                query : "?t="+time
-            },
-            {
-                reg : /^\/static\/(.*)$/i,
-                release : '/static/${product}/${namespace}/$1'
-            },
-            /*** 其它所有静态文件规则（将合并代码打包到res目录）*/
-            {
-                reg: /^.+$/,
-                release: '/res/${product}/${namespace}$&',
-                query : "?t="+time
-            }
-        ]
-    }
-};
+// module.exports = {
+//     project: {
+//         md5Length: 8,
+//         md5Connector: '.'
+//     },
+//     modules : {
+//         parser : {
+//             css : ["less"]
+//         },
+//         postpackager : "simple"
+//     },
+//     settings : {
+//         postprocessor : {
+//             jswrapper : {
+//                 type:'amd'
+//             }
+//         },
+//         optimizer : {
+//             'png-compressor' : {
+//                 type : 'pngquant',
+//                 speed : 2,
+//                 quality : [80, 100],
+//                 iebug : false
+//             },
+//             "uglify-js" : { 
+//                 mangle: {
+//                     except: "exports, module, require, define"
+//                 }
+//             }
+//         }
+//     },
+//     roadmap:{
+//         path:[
+//             /*** 模板编译规则（将所有html文件发布到view目录）*/
+//             {
+//                 reg : /^\/(page|layout|widget)\/(.+\.html)$/i,
+//                 isMod : true,
+//                 release : '/view/${product}/${namespace}/$1/$2',
+//                 id : '$1/$2'
+//             },
+//             /*** 静态文件编译规则（将page、layout、widget静态文件发布到res目录）*/
+//             {
+//                 reg : /^\/(page|layout|widget)\/(.*\.(js|css))$/i,
+//                 isMod : true,
+//                 release : '/res/${product}/${namespace}/$1/$2',
+//                 id : '$1/$2',
+//                 query : "?t="+time
+//             },
+//             /** page和widget doc目录下的ue设计图、mrd不需要编译产出 */
+//             {
+//                 reg : /^\/(page|widget)\/.*\/doc\/[^\.]*\.(psd|png|jpg|doc|md)/i,
+//                 release : false
+//             },
+//             /** 匹配static下的所有资源 */
+//             {
+//                 reg : /^\/static\/(.*\/ls-conf\.js)$/i,
+//                 release : '/static/${product}/${namespace}/$1',
+//                 isLSLib : true,
+//                 query : "?t="+time
+//             },
+//             {
+//                 reg : /^\/static\/(.*)$/i,
+//                 release : '/static/${product}/${namespace}/$1'
+//             },
+//             /*** 其它所有静态文件规则（将合并代码打包到res目录）*/
+//             {
+//                 reg: /^.+$/,
+//                 release: '/res/${product}/${namespace}$&',
+//                 query : "?t="+time
+//             }
+//         ]
+//     }
+// };
 
--- index.js
+++ index.js
@@ -3,26 +3,109 @@
  * @date   [2015-08]
  */
 
-var fis = module.exports = require("fis");
-var defaultConfig = require('./config/default.js');
+var fis = module.exports = require('fis3');
 
-fis.cli.name          = 'xbear';
-fis.require.prefixes  = ['xbear', 'fis'];
-fis.cli.help.commands = ['release', 'server'];
-fis.cli.info          = fis.util.readJSON(__dirname + '/package.json');
+fis.require.prefixes.unshift('xbear');
+fis.cli.name = 'xbear';
+fis.cli.info = require('./package.json');
 
-// @overide [重构 kernel/lib/compile]
-fis.compile = require('./lib/compile.js');
-// @overide [默认配置]
-fis.config.merge(defaultConfig);
+
+fis.match('*.{js,css,less,png,gif,jpg,doc,mod}', {
+    release: '/res/$0'  // 所有静态文件发布到 /res 目录下
+});
+
+fis.match('*.html', {
+    release: '/view/$0' // 所有模板文件发布到 /view 目录下
+});
+
+// 所有js, css 加 hash
+fis.match('*.{js,css,less}', {
+    useHash: false
+});
+
+// 所有图片加 hash
+fis.match('image', {
+    useHash: false
+});
+
+// fis-parser-less
+fis.match('*.less', {
+    parser: fis.plugin('less'),
+    rExt: '.css'
+});
+
+fis.match('*.js', {
+    optimizer: fis.plugin('uglify-js')
+});
+
+fis.match('*.{css,less}', {
+    optimizer: fis.plugin('clean-css')
+});
+
+fis.match('*.png', {
+    optimizer: fis.plugin('png-compressor')
+});
+
+fis.match('widget/*.{js,css}', {
+    isMod: true
+});
+
+fis.match('::package', {
+    spriter: fis.plugin('csssprites')
+});
+
+fis.hook('module', {
+    mode: 'amd'
+});
+
+// optimize
+fis.media('prod')
+    .match('*.js', {
+        optimizer: fis.plugin('uglify-js', {
+            mangle: {
+                expect: ['require', 'define', 'some string'] //不想被压的
+            }
+        })
+    })
+    .match('*.css', {
+        optimizer: fis.plugin('clean-css', {
+            'keepBreaks': true //保持一个规则一个换行
+        })
+    });
+
+// pack
+fis.media('prod')
+    // 启用打包插件，必须匹配 ::package
+    .match('::package', {
+        packager: fis.plugin('map'),
+        spriter: fis.plugin('csssprites', {
+            layout: 'matrix',
+            margin: '15'
+        })
+    })
+    .match('*.js', {
+        packTo: '/res/common_all.js'
+    })
+    .match('*.css', {
+        packTo: '/res/common_all.css'
+    })
+    .match('/widget/**/*.js', {
+        packTo: '/res/widget_all.js'
+    })
+    .match('/widget/**/*.css', {
+        packTo: '/res/widget_all.css'
+    });
+
 
 fis.cli.version = function() {
+    var v = fis.cli.info.version;
     var content = [
         '',
-        'v' + fis.cli.info.version,
+        'v' + v,
         '',
         '////////////////////////////////////////////'.bold.red,
         '/////              xBear               /////'.bold.yellow,
+        '/////              '.v.'               /////'.bold.yellow,
         '/////         全栈代码管理工具         /////'.bold.green,
         '/////       [dbxiao@foxmail.com]       /////'.bold.yellow,
         '///// http://develop.xpluse.com/xbear  /////'.bold.red,
@@ -31,6 +114,40 @@ fis.cli.version = function() {
     ].join('\n');
     console.log(content);
 };
+
+
+
+
+
+
+
+// var fis = module.exports = require("fis");
+// var defaultConfig = require('./config/default.js');
+// fis.cli.name          = 'xbear';
+// fis.require.prefixes  = ['xbear', 'fis'];
+// fis.cli.help.commands = ['release', 'server'];
+// fis.cli.info          = fis.util.readJSON(__dirname + '/package.json');
+
+// @overide [重构 kernel/lib/compile]
+// fis.compile = require('./lib/compile.js');
+// @overide [默认配置]
+// fis.config.merge(defaultConfig);
+
+// fis.cli.version = function() {
+//     var content = [
+//         '',
+//         'v' + fis.cli.info.version,
+//         '',
+//         '////////////////////////////////////////////'.bold.red,
+//         '/////              xBear               /////'.bold.yellow,
+//         '/////         全栈代码管理工具         /////'.bold.green,
+//         '/////       [dbxiao@foxmail.com]       /////'.bold.yellow,
+//         '///// http://develop.xpluse.com/xbear  /////'.bold.red,
+//         '////////////////////////////////////////////'.bold.blue,
+//         ''
+//     ].join('\n');
+//     console.log(content);
+// };
 
 
 
